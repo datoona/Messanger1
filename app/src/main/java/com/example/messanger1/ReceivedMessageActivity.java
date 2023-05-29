d0f7b385107b9bd250ec7302ac63f5b43b9ab53f
datoona <davit.arzumanyan92@gmail.com>
2023-05-29 18:25:09 +0400

    add two activity

---
+++ app/src/main/java/com/example/messanger1/ReceivedMessageActivity.java
@@ -0,0 +1,14 @@
+package com.example.messanger1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReceivedMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_received_message);
+    }
+}
